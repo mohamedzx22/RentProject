7ca74f7afba41adadd1a39f864d9821fc6315905
shahd mohamed emam <131971029+shahdemam@users.noreply.github.com>
2025-04-26 04:32:52 +0300

    Update AccountController.cs

--- Rent_Project/Rent_Project/Controllers/AccountController.cs
+++ Rent_Project/Rent_Project/Controllers/AccountController.cs
@@ -54,6 +54,16 @@ namespace Rent_Project.Controllers
             var result = await _accountService.LogoutAsync(User);
             return Ok(result);
         }
+         [HttpPost("refresh-token")]
+ public async Task<IActionResult> RefreshToken([FromBody] RefreshToken refreshToken )
+ {
+     var result = await _accountService.RefreshTokenAsync(refreshToken.Token);
+     if (result == null)
+         return Unauthorized("Invalid or expired refresh token");
+
+     return Ok(result);
+ }
+        
 
 
 
